24e8c32b197d3941de6cfcdfd46623b9dae23140
Pasha15357 <gavrilovpasha27@gmail.com>
2023-10-18 15:59:23 +0300

    Initial Commit

---
+++ Shared/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Shared
+//
+//  Created by Паша on 18.10.23.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
